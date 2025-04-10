6aafb1680eefa0cc1282202c7a1fa6698964d92e
pawn142 <fourfours1234@gmail.com>
2025-04-10 18:32:04 -0400

    Create piece-typedefs.ts

---
+++ piece-typedefs.ts
@@ -0,0 +1,31 @@
+import Fraction from './arithmetic.ts';
+
+enum Piece {
+    pawn = "pawn",
+    knight = "knight",
+    bishop = "bishop",
+    rook = "rook",
+    queen = "queen",
+    king = "king",
+}
+
+enum Side {
+    white = "white",
+    black = "black",
+}
+
+interface ColoredPiece {
+    piece: Piece;
+    side: Side;
+}
+
+interface IndexedPiece extends ColoredPiece {
+    index: number;
+}
+
+export {
+    Piece,
+    Side,
+    ColoredPiece,
+    IndexedPiece,
+}
