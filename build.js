5cf21e8371f517eb994479b0062686314a5c7672
inaccessibles <fourfours1234@gmail.com>
2025-05-08 15:34:53 -0400

    e

---
+++ build.js
@@ -0,0 +1,38 @@
+import ts from "typescript";
+import path from "path";
+
+const rootDir = './src';
+const outDir = './dist';
+
+const configPath = ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.json');
+
+const config = ts.parseJsonConfigFileContent(
+  ts.readConfigFile(configPath, ts.sys.readFile).config,
+  ts.sys,
+  path.dirname(configPath),
+  undefined,
+  configPath,
+);
+
+const program = ts.createProgram({
+  rootNames: config.fileNames,
+  options: {
+    ...config.options,
+    rootDir: rootDir,
+    outDir: outDir,
+    noEmit: false,
+  },
+});
+
+ts.getPreEmitDiagnostics(program).concat(program.emit().diagnostics).forEach(diagnostic => {
+  if (diagnostic.file) {
+    const { line, character } = ts.getLineAndCharacterOfPosition(
+      diagnostic.file,
+      diagnostic.start,
+    );
+    const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+    console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+  } else {
+    console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+  }
+});
